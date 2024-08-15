a0cddc195544f7bbd10776952d2760c2bfc83bdd
gson666 <86161190+gson666@users.noreply.github.com>
2024-08-15 12:44:09 +0300

    added frontend correctly

--- API/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ API/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1046efe482044015e371ac1b019c468c37137a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb64651c526af828d9ebef5086cb4caa0e04498")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
